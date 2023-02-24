791d2af2c7100105dae396763ba5a6710c5d8de1
DanielHarley <danielharleygoes@outlook.com>
2023-02-24 20:07:54 -0300

    (code) create snake class
    (code) create snake class:
    - create a snake class to be responsible for it's behavior and appearance.
    - create the "snake_segments" attribute, which is a "List[Turtle]", to match the snake's body.
    - create the method "move", which makes the snake navigate the UI.

---
+++ classes/snake.py
@@ -0,0 +1,28 @@
+from typing import List
+from turtle import Turtle
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake_segments: List[Turtle] = []
+
+        for index in range(3):
+            segment = Turtle(shape="square")
+            segment.color("white")
+            segment.penup()
+
+            x_position = -20 * index
+
+            segment.goto(x=x_position, y=0)
+            self.snake_segments.append(segment)
+
+    def move(self):
+        for segment_index in range(len(self.snake_segments) - 1, 0, -1):
+            new_x_position = self.snake_segments[segment_index - 1].xcor()
+            new_y_position = self.snake_segments[segment_index - 1].ycor()
+
+            self.snake_segments[segment_index].goto(x=new_x_position, y=new_y_position)
+
+        self.snake_segments[0].left(90)
+        self.snake_segments[0].forward(20)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from turtle import Screen, Turtle
-from typing import List
+from turtle import Screen
+from classes import snake as s
 import time
 
 screen = Screen()
@@ -8,19 +8,7 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-snake_segments: List[Turtle] = []
-
-for index in range(3):
-    segment = Turtle(shape="square")
-    segment.color("white")
-    segment.penup()
-
-    x_position = -20 * index
-
-    segment.goto(x=x_position, y=0)
-    snake_segments.append(segment)
-
-screen.update()
+snake = s.Snake()
 
 game_is_on = True
 
@@ -28,13 +16,6 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for segment_index in range(len(snake_segments) - 1, 0, -1):
-        new_x_position = snake_segments[segment_index - 1].xcor()
-        new_y_position = snake_segments[segment_index - 1].ycor()
-
-        snake_segments[segment_index].goto(x=new_x_position, y=new_y_position)
-
-    snake_segments[0].left(90)
-    snake_segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
